d2bfc864fed3236363ac26a6a8bc4ab11a940dac
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2023-05-22 15:20:21 +0200

    left slider v1 ok

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+
+
 # import tkinter as tk
 
 
@@ -17,7 +19,66 @@ class WUnique(ctk.CTk):
         # window name
         self.title("Unique")
 
-        self.file_menu = FileMenu(self)
+        self.file_menu = FileMenu(self, 20)
+        # game slide
+        self.game_panel = SlidePanel(self, -0.3, 0.0)
+        self.morpion_button = ctk.CTkButton(self.game_panel,
+                                            text='Morpion',
+                                            corner_radius=5)
+
+        self.morpion_button.pack(pady=5)
+
+        # main game button
+        self.button = ctk.CTkButton(self,
+                                    text="Game",
+                                    command=self.game_panel.move_panel)
+
+        self.button.place(relx=0.5,
+                          rely=0.5,
+                          anchor="center")
+
+
+class SlidePanel(ctk.CTkFrame):
+    def __init__(self, parent, start_pos, end_pos):
+        super().__init__(master=parent)
+
+        self.start_pos = start_pos  # -x
+        self.end_pos = end_pos  # 0
+        self.width = abs(start_pos - end_pos) + 0.03
+
+        self.pos = self.start_pos
+        self.is_hide = True
+        self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+        self.arrow = ctk.CTkButton(self,
+                                   text=">", width=20,
+                                   height=self.cget("height"),
+                                   fg_color="transparent",
+                                   command=self.move_panel)
+        self.arrow.pack(side="right", fill="y")
+
+    def move_panel(self):
+        if self.is_hide:
+            self.move_to_right()
+        else:
+            self.move_to_left()
+
+    def move_to_left(self):
+        if self.pos > self.start_pos:
+            self.pos -= 0.01
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.after(10, self.move_to_left)
+            self.arrow.configure(text=">")
+        else:
+            self.is_hide = True
+
+    def move_to_right(self):
+        if self.pos < self.end_pos:
+            self.pos += 0.01
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.after(10, self.move_to_right)
+            self.arrow.configure(text="<")
+        else:
+            self.is_hide = False
 
 
 class FileMenu:
@@ -25,29 +86,51 @@ class FileMenu:
     Menu "Fichier"
     """
 
-    def __init__(self, master):
-        # menu bar
+    def __init__(self, master, height):
+        # menu frame
         self.menu_frame = ctk.CTkFrame(master,
+                                       height=height,
+                                       corner_radius=0,
+                                       fg_color=("white", "black"))
+        self.menu_frame.place(x=0,
+                              y=0,
+                              relwidth=1)
+
+        # inside menu
+        self.options = ctk.CTkOptionMenu(self.menu_frame,
+                                         width=85,
+                                         height=20,
+                                         corner_radius=0,
+                                         values=["Aide"],
+                                         )  # put self.options.set("   Fichier") in callback function
+        self.options.place(x=0,
+                           y=0,
+                           )
+        # "title" menu
+        self.options.set("   Fichier")
+
+        # appearance menu
+        self.appearance_option = ctk.CTkOptionMenu(self.menu_frame,
+                                                   width=110,
+                                                   height=20,
+                                                   corner_radius=0,
+                                                   values=["Light", "Dark"],
+                                                   # fg_color=("white", "black"),
+                                                   command=self.optionmenu_callback,
+                                                   )
+        self.appearance_option.place(x=66,
+                                     y=0,
+                                     )
+        self.appearance_option.set("   Apparence")
+
+        # menu frame
+        self.menu_frame = ctk.CTkFrame(self.menu_frame,
                                        height=20,
                                        corner_radius=0,
-                                       fg_color=("white", "black"))  # color of menu frame TODO
-        self.menu_frame.place(relx=0,
-                              rely=0,
+                                       fg_color=("white", "black"))
+        self.menu_frame.place(x=156,
+                              y=0,
                               relwidth=1)
-        # help menu
-        self.menu_bar = ctk.CTkOptionMenu(self.menu_frame,
-                                          width=55,
-                                          height=20,
-                                          corner_radius=0,
-                                          # dropdown_fg_color="white",
-                                          # button_color=self.menu_frame.cget("fg_color"),
-                                          # button_hover_color=("blue"),
-                                          values=["Light", "Dark", "Aide"],
-                                          command=self.optionmenu_callback,
-                                          )
-        self.menu_bar.place(x=0,
-                            y=0, )
-        self.menu_bar.set("Fichier")
 
     def optionmenu_callback(self, choice):
         if choice == "Light" or "Dark":
@@ -55,7 +138,7 @@ class FileMenu:
 
     def change_appearance_to_light(self, choice):
         ctk.set_appearance_mode(choice)
-        self.menu_bar.set("Fichier")
+        self.appearance_option.set("   Apparence")
 
 
 test = WUnique()
